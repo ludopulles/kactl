a58c8a6e83d888c1c4d2193daa6535602b378564
Ludo Pulles <ludo.pulles@gmail.com>
2018-11-08 22:50:31 +0100

    Fix a warning....

--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -13,7 +13,7 @@ template<class T>
 struct Point {
 	typedef Point P;
 	T x, y;
-	explicit Point(T x=0, T y=0) : x(x), y(y) {}
+	explicit Point(T _x=0, T _y=0) : x(_x), y(_y) {}
 	bool operator<(P p) const { return tie(x,y) < tie(p.x,p.y); }
 	bool operator==(P p) const { return tie(x,y)==tie(p.x,p.y); }
 	P operator+(P p) const { return P(x+p.x, y+p.y); }
