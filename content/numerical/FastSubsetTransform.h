d1e7009d5d2452dc0474e7500e91141e981d3a15
Ludo Pulles <ludo.pulles@gmail.com>
2018-11-08 23:07:09 +0100

    Fix FST test, and try all 3 operators, by which the code has to compile
    Therefore, an operator-value is passed, making this a little bit slower, but still more flexible. Performance shouldn't differ that much....

--- content/numerical/FastSubsetTransform.h
+++ content/numerical/FastSubsetTransform.h
@@ -1,29 +1,29 @@
 /**
- * Author: Lucian Bicsi
+ * Author: Lucian Bicsi, Ludo Pulles (making it compile)
  * Date: 2015-06-25
  * License: GNU Free Documentation License 1.2
  * Source: csacademy
  * Description: Transform to a basis with fast convolutions of the form
  * $\displaystyle c[z] = \sum\nolimits_{z = x \oplus y} a[x] \cdot b[y]$,
- * where $\oplus$ is one of AND, OR, XOR. The size of $a$ must be a power of two.
+ * where $\oplus$ is one of AND (use \texttt{op < 0}), XOR (use \texttt{op = 0}) and OR (use \texttt{op > 0}). The size of $a$ must be a power of two.
  * Time: O(N \log N)
  * Status: tested
  */
 #pragma once
 
-void FST(vi& a, bool inv) {
+void FST(vi& a, int op, bool inv) {
 	for (int n = sz(a), step = 1; step < n; step *= 2) {
 		for (int i = 0; i < n; i += 2 * step) rep(j,i,i+step) {
-			int &u = a[j], &v = a[j + step]; tie(u, v) =
-				inv ? pii(v - u, u) : pii(v, u + v); // AND
-				inv ? pii(v, u - v) : pii(u + v, u); // OR
-				pii(u + v, u - v);                   // XOR
+			int &u = a[j], &v = a[j + step];
+			if (op<0) tie(u,v) = inv ? pii(v - u, u) : pii(v, u + v); // AND
+			if (op>0) tie(u,v) = inv ? pii(v, u - v) : pii(u + v, u); // OR
+			if (!op) tie(u, v) = pii(u + v, u - v); // XOR
 		}
 	}
-	if (inv) trav(x, a) x /= sz(a); // XOR only
+	if (inv && !op) trav(x, a) x /= sz(a);
 }
-vi conv(vi a, vi b) {
-	FST(a, 0); FST(b, 0);
-	rep(i,0,sz(a)) a[i] *= b[i];
-	FST(a, 1); return a;
+vi conv(vi a, vi b, int op) {
+	FST(a, op, 0); FST(b, op, 0);
+	rep(i, 0, sz(a)) a[i] *= b[i];
+	FST(a, op, 1); return a;
 }
--- fuzz-tests/fst.cpp
+++ fuzz-tests/fst.cpp
@@ -12,20 +12,37 @@ typedef vector<int> vi;
 
 #include "../content/numerical/FastSubsetTransform.h"
 
+const int maxk = 10, reps = 25;
+const int minval = -100, maxval = 100;
+
+int nextrand() {
+	return minval + rand() % (maxval - minval + 1);
+}
+
 int main() {
-	rep(k,0,10) {
-		vi a(1 << k), b = a, c = a, target = a;
-		trav(x, a) x = rand() % 6 - 2;
-		trav(x, b) x = rand() % 6 - 2;
-		rep(i,0,1 << k) rep(j,0,1 << k) target[i & j] += a[i] * b[j];
-		// rep(i,0,1 << k) cout << a[i] << ' '; cout << endl;
-		// rep(i,0,1 << k) cout << b[i] << ' '; cout << endl;
-		FST(a, false);
-		FST(b, false);
-		rep(i,0,1 << k) c[i] = a[i] * b[i];
-		FST(c, true);
-		// rep(i,0,1 << k) cout << c[i] << ' '; cout << endl;
-		// rep(i,0,1 << k) cout << target[i] << ' '; cout << endl;
-		assert(c == target);
+	srand(987632163);
+
+	// -1: AND, 0: OR, 1: XOR
+	for (int op : { -1, 0, 1 }) {
+		rep(_, 0, reps) rep(k, 0, maxk) {
+			int sz = 1 << k;
+			vi a(sz), b(sz);
+
+			// initialize with random data:
+			rep(i,0,sz) a[i] = nextrand(), b[i] = nextrand();
+
+			// do it the brute-force way:
+			vi expected(sz, 0);
+			rep(i,0,sz) rep(j,0,sz) {
+				int ij = op < 0 ? (i&j) : op ? (i|j) : i^j;
+				expected[ij] += a[i] * b[j];
+			}
+
+			// test the function
+			vi c = conv(a, b, op);
+
+			// compare results.
+			assert(c == expected);
+		}
 	}
 }
